e140068071506bd43e3b68a60b4b3c15be8f3996
dboiysxz <44053519+dboiysxz@users.noreply.github.com>
2021-03-11 20:33:00 +0100

    replacing hover effect with typing effect

--- main.js
+++ main.js
@@ -13,3 +13,33 @@ setInterval(() => {
     })
     jobItems[Math.floor(Math.random() * jobItems.length)].classList.add('active')
 }, 1000)
+
+const textElement = document.getElementById('text')
+       const words = textElement.dataset.words.split(',')
+       const typingSpeed = 100
+       const pauseTime = 2000
+       let count = 0
+
+       function typeWord(word) {
+           let typeCount = 0
+
+           const typeInterval = setInterval(() => {
+               if (typeCount <= word.length) {
+                   textElement.innerHTML = word.slice(0, typeCount) + '<span class="cursor">|</span>'
+                   typeCount ++
+               } else {
+                   clearInterval(typeInterval)
+                   setTimeout(() => {
+                       typeWord(nextWord())
+                   }, pauseTime)
+               }
+
+           }, typingSpeed)
+       }
+
+       function nextWord() {
+           count ++
+           return words[count % words.length]
+       }
+
+       typeWord(nextWord())
